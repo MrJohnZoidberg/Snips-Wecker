a10663a56b42b935c4de45b3deb4f3085f32e382
Dr. John Zoidberg <guenther.mueller.two@gmx.de>
2018-08-10 18:53:09 +0200

    Added cleaning action

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash -e
 
+sudo rm -rf /var/lib/snips/skills/.config
+sudo rm -rf /var/lib/snips/skills/.mplayer
+
 VENV=venv
 
 if [ ! -d "$VENV" ]
@@ -22,4 +25,9 @@ pip install -r requirements.txt
 if [ ! -f ./.saved_alarms ]; then
     touch .saved_alarms
     sudo chown _snips-skills .saved_alarms
+fi
+
+if [ ! -f ./.mplayer_pipe ]; then
+    touch .mplayer_pipe
+    sudo chown _snips-skills .mplayer_pipe
 fi
