d8adcecf7937599a02a959e45640124cc6b4795c
Dr. John Zoidberg <guenther.mueller.two@gmx.de>
2018-08-10 22:30:40 +0200

    Changed ringtone stopping method

--- alarmclock.py
+++ alarmclock.py
@@ -197,10 +197,11 @@ class AlarmClock:
         if self.ringing == 1:
             self.ringing = 0
             print("Stopping...")
+            self.player.terminate()
             #stdout_data = self.player.communicate(input=b"q")[0]  # send "q" key to mpg123 command
             #for i in range(6):
                 #self.player.communicate(input="+")
-            self.player.stdin.write("q")
+            #self.player.stdin.write(b"q")
                 #print("Volume higher...")
                 #time.sleep(1)
             self.ringing_timeout.cancel()
