2266b21aac2f336fe687fb2c8b5b29d18788b4ca
Dr. John Zoidberg <guenther.mueller.two@gmx.de>
2018-08-10 21:49:49 +0200

    Added debugging code

--- alarmclock.py
+++ alarmclock.py
@@ -196,6 +196,7 @@ class AlarmClock:
     def stop(self):
         if self.ringing == 1:
             self.ringing = 0
+            print("Stopping...")
             stdout_data = self.player.communicate(input=b"q")[0]  # send "q" key to mpg123 command
             self.ringing_timeout.cancel()
             return stdout_data
