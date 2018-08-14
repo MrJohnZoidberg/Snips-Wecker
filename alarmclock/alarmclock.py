df8428648ebe3065b346a27291be7432fce05e10
Dr. John Zoidberg <guenther.mueller.two@gmx.de>
2018-08-15 00:39:40 +0200

    Corrected typo

--- alarmclock/alarmclock.py
+++ alarmclock/alarmclock.py
@@ -52,8 +52,8 @@ class AlarmClock:
                     self.alarms[alarm_time] = {'siteId': alarm_site_id}  # add alarm to dict
                 dt = datetime.datetime
                 alarms_str_dict = {dt.strftime(dtobj, "%Y-%m-%d %H:%M"): self.alarms[dtobj] for dtobj in self.alarms}
-                self.mqtt_client.publish('external/alarmlock/newalarm', json.dumps({'new': {'datetime': alarm_time_str,
-                                                                                            'siteId': alarm_site_id},
+                self.mqtt_client.publish('external/alarmclock/newalarm', json.dumps({'new': {'datetime': alarm_time_str,
+                                                                                             'siteId': alarm_site_id},
                                                                                     'all': alarms_str_dict}))
                 return "Der Wecker wird {0} um {1} Uhr {2} klingeln.".format(ftime.get_future_part(alarm_time),
                                                                              ftime.get_alarm_hour(alarm_time),
